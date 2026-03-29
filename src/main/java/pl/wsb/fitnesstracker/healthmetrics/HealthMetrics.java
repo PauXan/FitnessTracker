390127c0a8eed3d9f6b3d537332512354f3574a8
PauXan <pauch.aleksander@gmail.com>
2026-03-29 17:23:56 +0200

    LAB02: Dodanie relacji JPA i pipeline CI/CD
      - Training: konwersja na @Entity z relacją @ManyToOne do User
      - Statistics: dodanie relacji @OneToOne do User, poprawienie nazw kolumn
      - HealthMetrics: zastąpienie surowego Long userId relacją @ManyToOne do User
      - User: dodanie brakujących kolumn firstName i lastName
      - Dodanie DatabaseSchemaTest do katalogu testowego
      - Dodanie pipeline CI/CD w .github/workflows/github-ci-cd.yml
      - Aktywacja profilu loadInitialData w application.properties

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -1,18 +1,21 @@
 package pl.wsb.fitnesstracker.healthmetrics;
 
 import jakarta.persistence.*;
+import pl.wsb.fitnesstracker.user.api.User;
+
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "Health_Metrics")
+@Table(name = "health_metrics")
 public class HealthMetrics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(name = "date", nullable = false)
     private LocalDate date;
@@ -29,8 +32,8 @@ public class HealthMetrics {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this.userId = userId; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 
     public LocalDate getDate() { return date; }
     public void setDate(LocalDate date) { this.date = date; }
